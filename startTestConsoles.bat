11df432f8f665cae86739c6ef09d36c5dbff7a0d
KonyPL <mrkony98@gmail.com>
2021-04-22 20:44:08 +0200

    Added batch file to ease development and debugging

--- startTestConsoles.bat
+++ startTestConsoles.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
 start cmd /k python server\server.py
-start cmd /k python client\client.py
+start cmd /k python client\client.py 6000 6001 Kony
+start cmd /k python client\client.py 6002 6003 Stmpl
