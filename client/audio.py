ba41b168096cf8e306aa3b026e0cfda3e82147c2
ElStempel <steplewski.aleks@gmail.com>
2021-05-01 14:50:27 +0200

    gui

--- client/audio.py
+++ client/audio.py
@@ -8,70 +8,6 @@ import client
 
 joined = False
 
-class Login_Okno(QMainWindow):
-    def __init__(self, *args, **kwargs):
-        super(Login_Okno,self).__init__(*args,*kwargs)
-        self.setWindowTitle("Dołączanie")
-        
-        #tytuł
-        titleText = QLabel()
-        titleText.setText("SquadTalk 4")
-        titleText.setAlignment(Qt.AlignCenter)
-        titleText.setStyleSheet("color: white")
-        titleText.setFont(QFont('Arial',45))
-        
-        #pole nazwy
-        self.nameField = QLineEdit()
-        self.nameField.setPlaceholderText("Podaj nazwę użytkownika")
-        self.nameField.setStyleSheet("color: white")
-        
-        #pole ip
-        self.ipField = QLineEdit()
-        self.ipField.setPlaceholderText("Podaj ip serwera")
-        self.ipField.setStyleSheet("color: white")
-        
-        #pole portu
-        self.portField = QLineEdit()
-        self.portField.setPlaceholderText("Podaj port serwera")
-        self.portField.setStyleSheet("color: white")
-        
-        #join button
-        joinButton = QPushButton()
-        joinButton.setText("Dołącz")
-        joinButton.setStyleSheet("background-color: rgb(0, 191, 178); color: white")
-        joinButton.clicked.connect(self.joinClicked)
-        
-        #save button
-        saveButton = QPushButton()
-        saveButton.setStyleSheet("background-color: rgb(72, 61, 63); color: white")
-        saveButton.setText("Zapisz ustawienia")
-        
-        #load button
-        loadButton = QPushButton()
-        loadButton.setStyleSheet("background-color: rgb(72, 61, 63); color: white")
-        loadButton.setText("Wczytaj ustawienia")
-        
-        #layout
-        mainMenu = QVBoxLayout()
-        mainMenu.addWidget(titleText)
-        mainMenu.addWidget(self.nameField)
-        mainMenu.addWidget(self.ipField)
-        mainMenu.addWidget(self.portField)
-        mainMenu.addWidget(joinButton)
-        mainMenu.addWidget(saveButton)
-        mainMenu.addWidget(loadButton)
-        mainMenu.setAlignment(Qt.AlignCenter)
-        
-        mainMenuW = QWidget()
-        mainMenuW.setLayout(mainMenu)
-        
-        self.setCentralWidget(mainMenuW)
-    
-    def joinClicked(self):
-        print("Join button clicked")
-        global joined
-        joined = True
-        
 class Stream_Okno(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(Stream_Okno,self).__init__(*args,*kwargs)
@@ -225,17 +161,3 @@ class Stream_Okno(QMainWindow):
         print("Leave button clicked")
         global joined
         joined = False
-
-app = QApplication(sys.argv)
-
-login_window = Login_Okno()
-login_window.setFixedSize(400,400)
-login_window.setStyleSheet("background-color: rgb(37,37,37)")
-login_window.show()
-
-stream_window = Stream_Okno()
-stream_window.setFixedSize(400,690)
-stream_window.setStyleSheet("background-color: rgb(37,37,37)")
-stream_window.show()
-
-app.exec_()
---
+++ client/main.py
@@ -0,0 +1,37 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+from PyQt5 import QtTest
+
+import audio, login, settings
+
+import sys
+import re, os
+
+class Okno(QMainWindow):
+    def __init__(self, *args, **kwargs):       
+        super(Okno, self).__init__(*args, *kwargs)
+        self.setWindowTitle("SquadTalk 4")
+        #self.setWindowIcon(QIcon('Icons/binary.png'))
+ 
+        self.tabs = QTabWidget()
+        self.tab1 = login.Login_Okno(self)
+        self.tab2 = audio.Stream_Okno(self)
+        self.tab3 = settings.Settings_Okno(self)
+        self.tabs.addTab(self.tab1, "Dołącz")
+        self.tabs.addTab(self.tab2, "Rozmawiaj")
+        self.tabs.addTab(self.tab3, "Ustawienia")
+        self.tabs.setStyleSheet("color: white")
+
+        self.setCentralWidget(self.tabs)
+
+#App and window initialization
+app = QApplication(sys.argv)
+
+window = Okno()
+window.setFixedSize(400, 750)
+window.setStyleSheet("background-color: rgb(37,37,37)")
+window.show()
+
+app.exec_()
